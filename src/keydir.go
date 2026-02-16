1378eb0c18a746b6bd5886a81a802e3ddbee8517
ibrahimcg <icg67253@gmail.com>
2026-02-16 10:14:45 +0300

    add keydir methods

--- src/keydir.go
+++ src/keydir.go
@@ -4,14 +4,27 @@ type KeyDir struct {
 	index map[string]*RecordMetadata
 }
 
+func NewKeyDir() *KeyDir {
+	return &KeyDir{
+		index: make(map[string]*RecordMetadata),
+	}
+}
+
 func (kd *KeyDir) Update(key string, metadata *RecordMetadata) {
+	kd.index[key] = metadata
 }
 
 func (kd *KeyDir) Get(key string) *RecordMetadata {
+	val, ok := kd.index[key]
+
+	if ok {
+		return val
+	}
 	return nil
 }
 
 func (kd *KeyDir) Remove(key string) {
+	delete(kd.index, key)
 }
 
 func (kd *KeyDir) RebuildFromFiles(files []*DataFile) {
