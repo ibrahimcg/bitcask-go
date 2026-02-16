454cdbea835f633273b6e8224b667bb15e6e3a41
ibrahimcg <icg67253@gmail.com>
2026-02-16 10:56:08 +0300

    add config

---
+++ src/config.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	Directory string `json:"directory"`
+}
+
+func LoadConfig(path string) (Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var cfg Config
+	if err := json.Unmarshal(data, &cfg); err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
